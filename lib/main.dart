a1de15b73370d91fe833a1f436e5d2d22252bf61
Ditto-Prasetyo <dittoprasetyo100@gmail.com>
2025-07-18 08:40:29 +0700

    fix: fixing the display on windows

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:bintar_sepuh/pages/auth/login.dart';
-import 'package:bintar_sepuh/pages/splashs/test.dart';
 import 'package:flutter/foundation.dart'; 
 import 'package:flutter/material.dart';
 import 'dart:io' show Platform;
--- web/web_entrypoint.dart
+++ web/web_entrypoint.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart' as ui;
 import 'package:bintar_sepuh/main.dart' as app;
 
 void main() {
